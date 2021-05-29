2d2cb7e1ac37305f0e29351e8b29cd0afc24b2d0
you will never know <81815804+saikr789@users.noreply.github.com>
2021-05-29 16:00:44 +0530

    Update create_top_files.py

--- create_top_files.py
+++ create_top_files.py
@@ -13,10 +13,13 @@ for days in [30,60,90,180,360,720]:
         df = df.drop(columns=["predicted"])
         df["minimum"] = df.apply(lambda row : row["actual"] if np.isnan(row["minimum"]) else row["minimum"], axis=1)
         df["maximum"] = df.apply(lambda row : row["actual"] if np.isnan(row["maximum"]) else row["maximum"], axis=1)
+        df[["minimum","maximum"]] = df.apply(lambda row : pd.Series([np.nanmin([row["minimum"],row["maximum"]]),np.nanmax([row["minimum"],row["maximum"]])]),axis=1)
         df["min"] = df["actual"] - df["minimum"]
         df["max"] = df["maximum"] - df["actual"]
-        buy = df.sort_values(by=["max"],ascending=[True])
-        sell = df.sort_values(by=["max"],ascending=[False])
+        df["suggest"] = df.apply(lambda row: "buy" if row["minimum"] <= row["actual"] <= row["minimum"] * 1.15 else "sell" if row["maximum"] * 0.85 <= row["actual"] <= row["maximum"] else None, axis=1)
+        sell = simdf.sort_values(by=["max"], ascending=[True])
+        buy = simdf.sort_values(by=["min"], ascending=[True])
+        df.to_csv(os.path.join(toppath,spath),index=None)
         buy.to_csv(os.path.join(toppath,"buy_"+str(days)+".csv"),index=None)
         sell.to_csv(os.path.join(toppath,"sell_"+str(days)+".csv"),index=None)
     except Exception as e:
